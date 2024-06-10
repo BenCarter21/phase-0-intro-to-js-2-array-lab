06480dce9d9235db2f86d8a82177a6d6b8bf5b14
Ben <bc@bencarterconsulting.com>
2024-06-10 14:56:42 -0400

    Assignment adjustments made.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+   cats.pop() 
+}
+function destructivelyRemoveFirstCat(){
+   cats.shift()
+}
+function appendCat(name) {
+    const extraCat = [...cats]
+    extraCat.push(name)
+    return extraCat
+}
+function prependCat(name) {
+    const extraCatTwo = [...cats]
+    extraCatTwo.unshift(name)
+    return extraCatTwo
+}
+function removeLastCat() {
+    const extraCatTwo = [...cats]
+    extraCatTwo.pop()
+    return extraCatTwo
+}
+function removeFirstCat() {
+    const extraCatTwo = [...cats]
+    extraCatTwo.shift()
+    return extraCatTwo
+}
